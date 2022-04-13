6c9c184573e254191d0f8286fbe8c30112f002d0
Tudor Coman <tcoman@adobe.com>
2022-04-13 11:22:24 +0300

    Boost install script

--- libs/build_dependencies.sh
+++ libs/build_dependencies.sh
@@ -10,6 +10,16 @@ OPENSSL_ROOT_DIR="/usr/local/opt/openssl"
 
 libDir=${1:-$DEFAULT_LIB_DIRECTORY_PATH}
 
+install_boost() {
+  if [ -d boost_1_78_0 ]; then
+      rm -rf boost_1_78_0
+  fi
+  curl -L -O https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz
+  tar -xzf boost_1_78_0.tar.gz
+  rm -rf boost_1_78_0.tar.gz
+  (cd boost_1_78_0 && ./bootstrap.sh && ./b2 && ./b2 install)    
+}
+
 install_cpprestsdk(){
 	restsdkDir="$libDir/cpprestsdk"
 	restsdkBuildDir="$restsdkDir/build.release"
@@ -32,14 +42,19 @@ install_postgresql(){
   postgresqlDir="$libDir/libpqxx"
   postgresqlBuildDir="$postgresqlDir/build.release"
 
+  if [ -d "$postgresqlDir" ]; then
+    rm -rf "$postgresqlDir"
+  fi
+
   git clone https://github.com/jtv/libpqxx "$postgresqlDir"
   (cd "$postgresqlDir" && git submodule update --init && git checkout tags/$PQXX_VERSION -b $PQXX_VERSION)
   mkdir "$postgresqlBuildDir"
   (cd "$postgresqlBuildDir" && cmake ../ -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF)
-  #(cd $postgresqlDir && ./configure && make && make install) || exit
+  (cd $postgresqlDir && ./configure && make && make install) || exit
   (cd "$postgresqlBuildDir" && make)
 }
 
 mkdir -p "$libDir"
+install_boost
 install_cpprestsdk
 install_postgresql
